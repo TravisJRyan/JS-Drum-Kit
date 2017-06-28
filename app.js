af75c7902d2149de8a87bfd35fe5a5b5730eb3cc
TravisJRyan <tryan1996@gmail.com>
2017-06-28 11:43:29 -0500

    documentation

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-window.addEventListener('keydown', function(e){
+window.addEventListener('keydown', function(e){ //event listener for key press
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 	console.log(audio);
 	if(!audio)
 		return;
-	audio.currentTime=0;
-	audio.play();
-	key.classList.add('playing');
+	audio.currentTime=0; //set audio time to 0 to help w/ fades
+	audio.play(); //play current audio file
+	key.classList.add('playing'); //add "playing" CSS class
 });
 
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+const keys = document.querySelectorAll('.key'); //get all keys
+keys.forEach(key => key.addEventListener('transitionend', removeTransition)); //add transition event for each key
 
-function removeTransition(e){
-	if(e.propertyName !== 'transform')
+function removeTransition(e){ //get rid of transition function
+	if(e.propertyName !== 'transform') 
 		return;
 	this.classList.remove('playing');
 
